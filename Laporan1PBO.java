e69e4c875c229886cc6b41a906978ea2adf6b1a2
nhawa <nhawa.c.ec@gmail.com>
2015-04-21 00:55:05 +0800

    laporan 1 PBO

---
+++ Laporan1PBO.java
@@ -0,0 +1,24 @@
+/*
+ * Nama : Anwar Sandi
+ * NIM  : 60200112032
+ * Kelas: B
+ */
+package laporan.pkg1.pbo;
+
+/**
+ *
+ * @author nhawa
+ */
+public class Laporan1PBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a = 2 + 1;
+        int b = a - 1;
+        System.out.println ("a = 2 + 1 = "+a);
+        System.out.println ("b = a - 1 = "+b);
+    }
+    
+}
