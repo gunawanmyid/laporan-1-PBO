4639e0e55719a0c971f02bf45ea4647ce4408bdf
Gunawan <gunawan.my.id@gmail.com>
2015-04-22 22:18:09 +0800

    Update Laporan1PBO.java
    lumayan, tapi sangat sederhana yah...^_^, silahkan di jawab pertanyaan saya di atas...^_^

--- Laporan1PBO.java
+++ Laporan1PBO.java
@@ -9,25 +9,25 @@ package laporan.pkg1.pbo;
  *
  * @author nhawa
  */
-public class Laporan1PBO {
+public class Laporan1PBO {  // BERIKAN PENJELASAN
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        biodata();
-        tambah();
+    public static void main(String[] args) {  // BERIKAN PENJELASAN
+        biodata();  // BERIKAN PENJELASAN
+        tambah();  // BERIKAN PENJELASAN
     }
     
-    public static void biodata(){
-        System.out.println ("Nama : Anwar Sandi");
+    public static void biodata(){ // BERIKAN PENJELASAN
+        System.out.println ("Nama : Anwar Sandi");  // BERIKAN PENJELASAN
         System.out.println ("NIM  : 60200112032");
         System.out.println ("Kelas: E");
     }
     
-    public static void tambah(){
-        int a = 2 + 1;
-        int b = a - 1;
+    public static void tambah(){  // BERIKAN PENJELASAN
+        int a = 2 + 1;  // BERIKAN PENJELASAN
+        int b = a - 1;  // BERIKAN PENJELASAN
         System.out.println ("a = 2 + 1 = "+a);
         System.out.println ("b = a - 1 = "+b);
     }
